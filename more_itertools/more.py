42e20e80093b1ecacf303a3ab96a69624b7efd32
Jason R. Coombs <jaraco@jaraco.com>
2014-04-05 21:28:11 -0400

    Add always_iterable from jaraco.util

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -3,7 +3,7 @@ from itertools import izip_longest
 from recipes import *
 
 __all__ = ['chunked', 'first', 'peekable', 'collate', 'consumer', 'ilen',
-           'iterate', 'with_iter']
+           'iterate', 'with_iter', 'always_iterable']
 
 
 _marker = object()
@@ -235,3 +235,40 @@ def with_iter(context_manager):
     with context_manager as iterable:
         for item in iterable:
             yield item
+
+
+def always_iterable(item):
+    """
+    Given an object, always return an iterable. If the item is not
+    already iterable, return a tuple containing only the item. If item is
+    None, an empty iterable is returned. Strings are not considered iterable.
+
+    >>> always_iterable([1,2,3])
+    [1, 2, 3]
+
+    >>> always_iterable('foo')
+    ('foo',)
+
+    >>> always_iterable(None)
+    ()
+
+    >>> always_iterable(range(10))
+    range(0, 10)
+
+    >>> def _test_func(): yield "I'm iterable"
+    >>> print(next(always_iterable(_test_func())))
+    I'm iterable
+
+    This function is particularly useful in simplifying the case where a
+    parameter could be iterable but may be None or a single value:
+
+        def process_codes(code):
+            "code may be None, str, or list of strings"
+            for item in always_iterable(code):
+                process(item)
+    """
+    if item is None:
+        item = ()
+    if isinstance(item, six.string_types) or not hasattr(item, '__iter__'):
+        item = item,
+    return item
