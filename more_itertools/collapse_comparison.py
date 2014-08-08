82fb2927fcea350669ca9336eec64cdbb42a2e01
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-08 09:23:37 -0700

    PEP8 changes

--- more_itertools/collapse_comparison.py
+++ more_itertools/collapse_comparison.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     num_repeat = 5
     number = 1000
     setup = """
-from recipes import flatten, flatten_2, flatten_3, flatten_4, collapse, collapse2, collapse3
+from recipes import flatten, flatten_2, flatten_3, flatten_4
+from recipes import collapse, collapse2, collapse3
 f0 = [[0, 1, 2], [3, 4, 5]]
 f1 = [[0, [1, 2]], [[3, 4], 5]]
     """
@@ -32,130 +33,130 @@ f1 = [[0, [1, 2]], [[3, 4], 5]]
         print '\n===Create generator (1000 times, in nanoseconds)==='
         print "flatten:  ",
         print min(timeit.repeat(
-            stmt='flatten('+test_iterable+')',
+            stmt='flatten(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten 2:",
         print min(timeit.repeat(
-            stmt='flatten_2('+test_iterable+')',
+            stmt='flatten_2(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten_3:",
         print min(timeit.repeat(
-            stmt='flatten_3('+test_iterable+')',
+            stmt='flatten_3(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten_4:",
         print min(timeit.repeat(
-            stmt='flatten_4('+test_iterable+')',
+            stmt='flatten_4(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse: ",
         print min(timeit.repeat(
-            stmt='collapse('+test_iterable+')',
+            stmt='collapse(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse2: ",
         print min(timeit.repeat(
-            stmt='collapse2('+test_iterable+')',
+            stmt='collapse2(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
 
         print "collapse3: ",
         print min(timeit.repeat(
-            stmt='collapse3('+test_iterable+')',
+            stmt='collapse3(' + test_iterable + ')',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
 
         print '\n===Use generator to create a list (1000 times, in nanoseconds)==='
         print "flatten:  ",
         print min(timeit.repeat(
-            stmt='list(flatten('+test_iterable+'))',
+            stmt='list(flatten(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten 2:",
         print min(timeit.repeat(
-            stmt='list(flatten_2('+test_iterable+'))',
+            stmt='list(flatten_2(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten_3:",
         print min(timeit.repeat(
-            stmt='list(flatten_3('+test_iterable+'))',
+            stmt='list(flatten_3(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "flatten_4:",
         print min(timeit.repeat(
-            stmt='list(flatten_4('+test_iterable+'))',
+            stmt='list(flatten_4(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse: ",
         print min(timeit.repeat(
-            stmt='list(collapse('+test_iterable+'))',
+            stmt='list(collapse(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse2:",
         print min(timeit.repeat(
-            stmt='list(collapse2('+test_iterable+'))',
+            stmt='list(collapse2(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse3:",
         print min(timeit.repeat(
-            stmt='list(collapse3('+test_iterable+'))',
+            stmt='list(collapse3(' + test_iterable + '))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse2 w/ levels = 0:",
         print min(timeit.repeat(
-            stmt='list(collapse2('+test_iterable+', levels=0))',
+            stmt='list(collapse2(' + test_iterable + ', levels=0))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse3 w/ levels = 0:",
         print min(timeit.repeat(
-            stmt='list(collapse3('+test_iterable+', levels=0))',
+            stmt='list(collapse3(' + test_iterable + ', levels=0))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse2 w/ levels = 1:",
         print min(timeit.repeat(
-            stmt='list(collapse2('+test_iterable+', levels=1))',
+            stmt='list(collapse2(' + test_iterable + ', levels=1))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
         print "collapse3 w/ levels = 1:",
         print min(timeit.repeat(
-            stmt='list(collapse3('+test_iterable+', levels=1))',
+            stmt='list(collapse3(' + test_iterable + ', levels=1))',
             setup=setup,
             number=number,
             repeat=num_repeat
-            )) * 1000
+        )) * 1000
