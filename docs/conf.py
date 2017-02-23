6a552e6bd5b9b43ec3f65047b7992cbc09f155d0
Bo Bayles <bbayles@gmail.com>
2017-02-22 21:20:35 -0600

    Bump version to 2.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, Erik Rose'
 # built documents.
 #
 # The short X.Y version.
-version = '2.5.0'
+version = '2.6.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
