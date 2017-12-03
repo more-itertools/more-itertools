fdbf5ab645dc6d5d447102537e49091033341c1c
Bo Bayles <bbayles@gmail.com>
2017-12-02 20:38:59 -0600

    Fix docs display on PyPI (4.0.1)

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ copyright = u'2012, Erik Rose'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0.0'
+version = '4.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
