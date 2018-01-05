376cf8b286ff3ef6aebf15f4832a05ee9d1a7ea2
michael-celani <michael.j.celani@gmail.com>
2018-01-05 00:42:14 -0600

    Remove roundrobin from branch

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -22,7 +22,7 @@ from sys import maxsize, version_info
 from six import binary_type, string_types, text_type
 from six.moves import filter, map, range, zip, zip_longest
 
-from .recipes import consume, flatten, take, roundrobin
+from .recipes import consume, flatten, take
 
 __all__ = [
     'adjacent',
@@ -805,16 +805,6 @@ def interleave_longest(*iterables):
     """
     i = chain.from_iterable(zip_longest(*iterables, fillvalue=_marker))
     return filter(lambda x: x is not _marker, i)
-
-
-# The performance of roundrobin is far superior to interleave_longest in PyPy
-if python_implementation() == 'PyPy':
-    _interleave_longest_docstring = interleave_longest.__doc__
-
-    def interleave_longest(*iterables):
-        return roundrobin(*iterables)
-
-    interleave_longest.__doc__ = _interleave_longest_docstring
 
 
 def collapse(iterable, base_type=None, levels=None):
