a1c386de575be374dbb3feee9dafb02a717fb626
Bo Bayles <bbayles@gmail.com>
2018-08-06 19:52:12 -0500

    Note on duplicates with powerset

--- more_itertools/recipes.py
+++ more_itertools/recipes.py
@@ -343,8 +343,20 @@ def partition(pred, iterable):
 def powerset(iterable):
     """Yields all possible subsets of the iterable.
 
-        >>> list(powerset([1,2,3]))
+        >>> list(powerset([1, 2, 3]))
         [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
+
+    :func:`powerset` will operate on iterables that aren't :class:`set`s,
+    so repeated elements in the input will produce repeated elements in the
+    output. Use :func:`unique_everseen` on the input to avoid generating
+    duplicates:
+
+        >>> seq = [1, 1, 0]
+        >>> list(powerset(seq))
+        [(), (1,), (1,), (0,), (1, 1), (1, 0), (1, 0), (1, 1, 0)]
+        >>> from more_itertools import unique_everseen
+        >>> list(powerset(unique_everseen(seq)))
+        [(), (1,), (0,), (1, 0)]
 
     """
     s = list(iterable)
