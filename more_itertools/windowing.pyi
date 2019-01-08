13427953149e38f305bb200f66fe1b0d9463cbc0
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-08 02:16:59 +0200

    Add stubs for windowing functions
    Just realize I missed a bunch of recipies in the last commits, oh well - TODO!

---
+++ more_itertools/windowing.pyi
@@ -0,0 +1,36 @@
+from typing import *
+
+T = TypeVar("T")
+U = TypeVar("U")
+@overload
+def windowed(
+    seq: Sequence[T], n: int, fillvalue: None, step: int = ...
+) -> Iterator[Tuple[T, ...]]: ...
+@overload
+def windowed(
+    seq: Sequence[T], n: int, fillvalue: None, step: int = 0
+) -> Iterator[Tuple]: ...
+@overload
+def windowed(
+    seq: Sequence[T], n: int, fillvalue: U, step: int = ...
+) -> Iterator[Tuple[Union[T, U]]]: ...
+
+# TODO: not sure about this one. also maybe rename it to subiters because it doesn't have to be a string
+def substrings(iterable: Iterable[T]) -> Iterator[T]: ...
+@overload
+def stagger(
+    iterable: Iterable[T],
+    offsets: Tuple[int, ...] = ...,
+    longest: bool = ...,
+    fillvalue: None = ...,
+) -> Iterable[Tuple[Union[T, None], ...]]: ...
+@overload
+def stagger(
+    iterable: Iterable[T],
+    offsets: Tuple[int, ...] = ...,
+    longest: bool = ...,
+    fillvalue: U = ...,
+) -> Iterable[Tuple[Union[T, U], ...]]: ...
+
+# Recipies
+def pairwise(iterable: Iterable[T]) -> Iterator[Tuple[T, T]]: ...
