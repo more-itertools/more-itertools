4836701f99e0ffe46c371b07bd5174a2f76dc380
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-11-30 18:56:10 +0100

    Fix typo in documentation

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -2463,7 +2463,7 @@ def difference(iterable, func=sub, *, initial=None):
     set to something other than ``None``, it will skip the first element when
     computing successive differences.
 
-        >>> iterable = [100, 101, 103, 106]  # accumate([1, 2, 3], initial=100)
+        >>> iterable = [100, 101, 103, 106]  # accumulate([1, 2, 3], initial=100)
         >>> list(difference(iterable, initial=100))
         [1, 2, 3]
 
