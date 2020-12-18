fe29b35faf23c2a2d95a633c6124ac3241adee65
Bo Bayles <bbayles@gmail.com>
2020-12-17 20:35:56 -0600

    Add timed_out property for time_limited

--- tests/test_more.py
+++ tests/test_more.py
@@ -3674,16 +3674,25 @@ class TimeLimitedTests(TestCase):
             sleep(0.2)
             yield 3
 
-        iterable = generator()
-        actual = list(mi.time_limited(0.1, iterable))
+        iterable = mi.time_limited(0.1, generator())
+        actual = list(iterable)
         expected = [1, 2]
         self.assertEqual(actual, expected)
+        self.assertTrue(iterable.timed_out)
+
+    def test_complete(self):
+        iterable = mi.time_limited(2, iter(range(10)))
+        actual = list(iterable)
+        expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(actual, expected)
+        self.assertFalse(iterable.timed_out)
 
     def test_zero_limit(self):
-        iterable = count()
-        actual = list(mi.time_limited(0, iterable))
+        iterable = mi.time_limited(0, count())
+        actual = list(iterable)
         expected = []
         self.assertEqual(actual, expected)
+        self.assertTrue(iterable.timed_out)
 
     def test_invalid_limit(self):
         with self.assertRaises(ValueError):
