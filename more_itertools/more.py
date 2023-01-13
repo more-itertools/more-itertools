bcd5323a39228adfc97537c363f06b78b5dbf036
Bo Bayles <bbayles@gmail.com>
2023-01-13 14:04:43 -0600

    Add gray_product to docs

--- more_itertools/more.py
+++ more_itertools/more.py
@@ -4358,33 +4358,34 @@ def gray_product(*iterables):
         >>> list(gray_product('AB','CD'))
         [('A', 'C'), ('B', 'C'), ('B', 'D'), ('A', 'D')]
 
-    This is known under several names: "n-ary", "non-Boolean", "non-binary",
-    or "mixed-radix" Gray code.
+    This function consumes all of the input iterables before producing output.
+    If any of the input iterables have fewer than two items, ``ValueError`` is raised.
 
-    All input iterables are consumed by this function before the first output
-    item can be generated. Each iterable must have at least two items, or a
-    ``ValueError`` is raised.
-
-    Reference: Knuth's "The Art of Computer Programming", Pre-Fascicle 2A,
-    "A Draft of Section 7.2.1.1: Generating all n-Tuples", Page 20,
-    Algorithm H, available at
-    https://www-cs-faculty.stanford.edu/~knuth/fasc2a.ps.gz
+    For information on the algorithm, see
+    `this section <https://www-cs-faculty.stanford.edu/~knuth/fasc2a.ps.gz>`__
+    of Donald Knuth's *The Art of Computer Programming*.
     """
-    m = tuple(tuple(x) for x in iterables)
-    for x in m:
-        if len(x) < 2:
+    all_iterables = tuple(tuple(x) for x in iterables)
+    iterable_count = len(all_iterables)
+    for iterable in all_iterables:
+        if len(iterable) < 2:
             raise ValueError("each iterable must have two or more items")
-    a = [0] * len(m)
-    f = list(range(len(m) + 1))
-    o = [1] * len(m)
+
+    # This is based on "Algorithm H" from section 7.2.1.1, page 20.
+    # a holds the indexes of the source iterables for the n-tuple to be yielded
+    # f is the array of "focus pointers" 
+    # o is the array of "directions" 
+    a = [0] * iterable_count
+    f = list(range(iterable_count + 1))
+    o = [1] * iterable_count
     while True:
-        yield tuple(m[i][a[i]] for i in range(len(m)))
+        yield tuple(all_iterables[i][a[i]] for i in range(iterable_count))
         j = f[0]
         f[0] = 0
-        if j == len(m):
+        if j == iterable_count:
             break
         a[j] = a[j] + o[j]
-        if a[j] == 0 or a[j] == len(m[j]) - 1:
+        if a[j] == 0 or a[j] == len(all_iterables[j]) - 1:
             o[j] = -o[j]
             f[j] = f[j + 1]
             f[j + 1] = j + 1
