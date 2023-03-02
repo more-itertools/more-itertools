ea47f251de92c5b2ed7d44e1544785cd0c7dcb50
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2023-03-02 13:32:23 +0100

    Remove testing script for development

--- dev_comb.py
+++
@@ -1,9 +0,0 @@
-from more_itertools import combination_with_replacement_index
-from itertools import combinations_with_replacement
-
-
-print(combination_with_replacement_index((1, 2), range(6)))
-print(combination_with_replacement_index("abc", "abcdef"))
-print(combination_with_replacement_index("aaa", "abcdef"))
-print(combination_with_replacement_index("bbc", "abcdef"))
-print(combination_with_replacement_index("ggg", "abcdef"))
