f3c5965ec8af5facd77984e5f675bb63ee06d894
Raymond Hettinger <raymond.hettinger@gmail.com>
2025-08-20 23:01:53 -0500

    Missing word

--- more_itertools/recipes.py
+++ more_itertools/recipes.py
@@ -1077,7 +1077,7 @@ def reshape(matrix, shape):
     Flattening stops when the first value in a dimension is a scalar.
     Scalars are bytes, strings, and non-iterables.
     The reshape iterator stops when the requested shape is complete
-    or when the input exhausted, whichever comes first.
+    or when the input is exhausted, whichever comes first.
 
     """
     if isinstance(shape, int):
