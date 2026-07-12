d992be0de9383ddcaae3a24866a2d96b52132b07
Raymond Hettinger <raymond.hettinger@gmail.com>
2026-07-12 15:05:04 -0500

    Fix stability in running_min and running_max

--- more_itertools/recipes.py
+++ more_itertools/recipes.py
@@ -1471,14 +1471,25 @@ def running_mean(iterable, *, maxlen=None):
 
 
 def _windowed_running_min(iterator, maxlen):
-    sis = deque()  # Strictly increasing subsequence
+    # Monotonically increasing subsequence of potential minimums,
+    # ordered by arrival time, with the window minimum at s[0]
+    # and the newest value at s[-1].
+    s = deque()
+
     for index, value in enumerate(iterator):
-        if sis and sis[0][0] == index - maxlen:
-            sis.popleft()
-        while sis and not sis[-1][1] < value:  # Remove non-increasing values
-            sis.pop()
-        sis.append((index, value))  # Most recent value at position -1
-        yield sis[0][1]  # Window minimum at position 0
+        # Has the current minimum aged out?
+        if s and s[0][0] == index - maxlen:
+            s.popleft()
+
+        # Remove decreasing values
+        while s and not s[-1][1] <= value:
+            s.pop()
+
+        # Place newest value at the end
+        s.append((index, value))
+
+        # Current minimum is at position zero
+        yield s[0][1]
 
 
 def running_min(iterable, *, maxlen=None):
@@ -1512,14 +1523,25 @@ def running_min(iterable, *, maxlen=None):
 
 
 def _windowed_running_max(iterator, maxlen):
-    sds = deque()  # Strictly decreasing subsequence
+    # Monotonically decreasing subsequence of potential maximums,
+    # ordered by arrival time, with the window maximum at s[0]
+    # and the newest value at s[-1].
+    s = deque()
+
     for index, value in enumerate(iterator):
-        if sds and sds[0][0] == index - maxlen:
-            sds.popleft()
-        while sds and not sds[-1][1] > value:  # Remove non-decreasing values
-            sds.pop()
-        sds.append((index, value))  # Most recent value at position -1
-        yield sds[0][1]  # Window maximum at position 0
+        # Has the current maximum aged out?
+        if s and s[0][0] == index - maxlen:
+            s.popleft()
+
+        # Remove increasing values
+        while s and not s[-1][1] >= value:
+            s.pop()
+
+        # Place newest value at the end
+        s.append((index, value))
+
+        # Current maximum is at position zero
+        yield s[0][1]
 
 
 def running_max(iterable, *, maxlen=None):
